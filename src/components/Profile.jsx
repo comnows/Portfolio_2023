0238bb0201af9439aca5988ed942033e1f39001a
comnows <watthanandh.l@mail.kmutt.ac.th>
2023-06-29 13:44:38 +0700

    ADD Profile section design

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,20 @@
+import myProfilePic from "../assets/imgs/profile/my-profile-pic-1.jpg";
+
 const Profile = () => {
-  return <div>My name is Now kub</div>;
+  return (
+    <div className="flex items-center justify-center bg-hawkes-blue rounded-60px h-full ml-2 mr-2">
+      <div className="flex flex-row lg:gap-20 2xl:gap-24 p-20">
+        <div className="flex flex-col justify-center text-6xl font-semibold text-midnight-blue">
+          <div>Hi I'm</div>
+          <div>Watthanandh Luaengtawekul</div>
+          <div className=" text-2xl">who am i what i do</div>
+        </div>
+        <div className="w-96 lg:w-96 m-9">
+          <img src={myProfilePic} alt="" className="rounded-60px" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    borderRadius: {
+      '60px': '60px'
+    },
     extend: {
       colors: {
         'blue-charcoal': '#04031e',
-        'dark-royal-blue': '#02066f'
+        'dark-royal-blue': '#02066f',
+        'hawkes-blue': '#cbe5ff',
+        'midnight-blue': '#011d32'
       }
     },
   },
