6d2c89c9a1ee0cb6d04f845108c413eb5981be66
comnows <watthanandh.l@mail.kmutt.ac.th>
2023-07-07 13:27:36 +0700

    ADD Works section design

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,106 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 import myProfilePic from "../assets/imgs/profile/my-profile-pic-1.jpg";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const slideData = [
+  {
+    id: 1,
+    name: "PlantPlay",
+    path: "https://www.youtube.com/embed/2o48N3CODHU",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 2,
+    name: "PlantPlay",
+    path: "https://www.youtube.com/embed/2o48N3CODHU",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 3,
+    name: "PlantPlay",
+    path: "https://www.youtube.com/embed/2o48N3CODHU",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 4,
+    name: "PlantPlay",
+    path: "https://www.youtube.com/embed/2o48N3CODHU",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
 
 const Projects = () => {
   return (
     <div className="bg-black-pearl rounded-60px h-full m-2">
-      <div className="flex pt-7 pl-11">
+      <div className="flex pt-11 pb-14 pl-16">
         <div className="flex flex-col w-full h-full">
-          {/* <div className=" text-aqua-green font-semibold uppercase">
+          <div className=" text-aqua-green text-xl font-semibold uppercase">
             what i do
           </div>
-          <div className="flex justify-center">
+          <Swiper
+            modules={[Navigation, Pagination]}
+            navigation
+            pagination={{ clickable: true }}
+            spaceBetween={10}
+            slidesPerView={1}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+            className="w-full"
+          >
+            {slideData.map((data) => (
+              <SwiperSlide key={data.id}>
+                <div className="container text-aqua-green mx-auto mt-4 mb-6">
+                  {data.path.includes("https") ? (
+                    <iframe
+                      src={data.path}
+                      title={data.name + "preview video"}
+                      allowFullScreen
+                      className="w-1/2 aspect-video mx-auto mb-6"
+                    ></iframe>
+                  ) : (
+                    <img src={data.path}></img>
+                  )}
+                  <div className="text-center text-2xl font-semibold mb-2">
+                    {data.name}
+                  </div>
+                  <div className="text-center text-xl">{data.description}</div>
+                </div>
+              </SwiperSlide>
+            ))}
+            {/* <SwiperSlide>
+              <div className="container text-aqua-green mx-auto mt-4 mb-6">
+                <iframe
+                  src="https://www.youtube.com/embed/2o48N3CODHU"
+                  title="YouTube video player"
+                  allowFullScreen
+                  className="w-1/2 aspect-video mx-auto mb-6"
+                ></iframe>
+                <div className="text-center text-2xl font-semibold mb-2">
+                  PlantPlay
+                </div>
+                <div className="text-center text-xl">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide> */}
+          </Swiper>
+
+          {/* <Swiper modules={[Navigation]} navigation slidesPerView={1} loop>
+            <SwiperSlide>Slide 1</SwiperSlide>
+          </Swiper> */}
+          {/* <div className="flex justify-center">
             <img src={myProfilePic} alt="" />
           </div> */}
         </div>
